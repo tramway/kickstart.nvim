89b8f0570d0ef3e2a7280724e2a86bbdb7a67acf
Ruslan Rakhmanov <ruslan.rakhmanov.str@gmail.com>
2026-05-24 15:07:55 +0200

    ts-tools improvements

--- init.lua
+++ init.lua
@@ -318,8 +318,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('v', '<leader>sv', builtin.grep_string, { desc = '[S]earch current [V]isual selection' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord or selection' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
@@ -389,7 +388,20 @@ require('lazy').setup({
   {
     'pmizio/typescript-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-    opts = {},
+    opts = {
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = 'all',
+          includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+    },
   },
 
   {
@@ -934,7 +946,7 @@ require('lazy').setup({
       }
 
       -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
+      --  rou could remove this setup call if you don't like it,
       --  and try some other statusline plugin
       local statusline = require 'mini.statusline'
       -- set use_icons to true if you have a Nerd Font
@@ -1059,7 +1071,7 @@ require('lazy').setup({
         uppercase_labels = true,
       }
       local directions = require('hop.hint').HintDirection
-      vim.keymap.set('', 's', function() hop.hint_words {} end, { remap = true })
+      vim.keymap.set('', '\\', function() hop.hint_words {} end, { remap = true })
     end,
   },
 
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -39,129 +39,93 @@ return {
     -- TODO: change lua formatter to keep such entries in one line
     {
       '<leader>dB',
-      function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end,
+      function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end,
       desc = 'Breakpoint Condition',
     },
     {
       '<leader>db',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
+      function() require('dap').toggle_breakpoint() end,
       desc = 'Toggle Breakpoint',
     },
     {
       '<leader>dc',
-      function()
-        require('dap').continue()
-      end,
+      function() require('dap').continue() end,
       desc = 'Run/Continue',
     },
     {
       '<leader>da',
-      function()
-        require('dap').continue { before = get_args }
-      end,
+      function() require('dap').continue { before = get_args } end,
       desc = 'Run with Args',
     },
     {
       '<leader>dC',
-      function()
-        require('dap').run_to_cursor()
-      end,
+      function() require('dap').run_to_cursor() end,
       desc = 'Run to Cursor',
     },
     {
       '<leader>dg',
-      function()
-        require('dap').goto_()
-      end,
+      function() require('dap').goto_() end,
       desc = 'Go to Line (No Execute)',
     },
     {
       '<leader>di',
-      function()
-        require('dap').step_into()
-      end,
+      function() require('dap').step_into() end,
       desc = 'Step Into',
     },
     {
       '<leader>dj',
-      function()
-        require('dap').down()
-      end,
+      function() require('dap').down() end,
       desc = 'Down',
     },
     {
       '<leader>dk',
-      function()
-        require('dap').up()
-      end,
+      function() require('dap').up() end,
       desc = 'Up',
     },
     {
       '<leader>dl',
-      function()
-        require('dap').run_last()
-      end,
+      function() require('dap').run_last() end,
       desc = 'Run Last',
     },
     {
       '<leader>do',
-      function()
-        require('dap').step_out()
-      end,
+      function() require('dap').step_out() end,
       desc = 'Step Out',
     },
     {
       '<leader>dO',
-      function()
-        require('dap').step_over()
-      end,
+      function() require('dap').step_over() end,
       desc = 'Step Over',
     },
     {
       '<leader>dP',
-      function()
-        require('dap').pause()
-      end,
+      function() require('dap').pause() end,
       desc = 'Pause',
     },
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     {
       '<leader>dl',
-      function()
-        require('dapui').toggle()
-      end,
+      function() require('dapui').toggle() end,
       desc = 'Debug: See last session result.',
     },
     {
       '<leader>dr',
-      function()
-        require('dap').repl.toggle()
-      end,
+      function() require('dap').repl.toggle() end,
       desc = 'Toggle REPL',
     },
     {
       '<leader>ds',
-      function()
-        require('dap').session()
-      end,
+      function() require('dap').session() end,
       desc = 'Session',
     },
     {
       '<leader>dt',
-      function()
-        require('dap').terminate()
-      end,
+      function() require('dap').terminate() end,
       desc = 'Terminate',
     },
     {
       '<leader>dw',
-      function()
-        require('dap.ui.widgets').hover()
-      end,
+      function() require('dap.ui.widgets').hover() end,
       desc = 'Widgets',
     },
   },
@@ -182,7 +146,7 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
       },
     }
 
@@ -333,12 +297,12 @@ return {
     end
 
     -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
+    -- require('dap-go').setup {
+    --   delve = {
+    --     -- On Windows delve must be run attached or it crashes.
+    --     -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+    --     detached = vim.fn.has 'win32' == 0,
+    --   },
+    -- }
   end,
 }
